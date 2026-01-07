c0c46ac5bc8c119125e1d3df1fca3a474b35be89
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2026-01-08 03:40:45 +0700

    fix: use IMAGE_NAME

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -17,7 +17,7 @@ docker run --init --rm --network=host \
     -e DISPLAY=host.docker.internal:0 \
     --name vivado \
     --mount type=bind,source="$script_dir/../",target="/home/user" \
-    --platform linux/amd64 ubuntu_vivado_env \
+    --platform linux/amd64 $IMAGE_NAME \
     sudo -H -u user bash scripts/startup.sh &
 
 # monitor vivado container
